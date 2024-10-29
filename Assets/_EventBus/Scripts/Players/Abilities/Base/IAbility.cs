af60bcec11cfa652bc1777bb07c1de03b3ec7b41
Ponyu-dev <mopoypa@yandex.ru>
2024-10-29 15:07:22 +0100

    Init for work

--- Assets/_EventBus/Scripts/Players/Abilities/Base/IAbility.cs
+++
@@ -1,9 +0,0 @@
-namespace _EventBus.Scripts.Players.Abilities.Base
-{
-    public interface IAbility { }
-    public interface IAbilityTurnStart : IAbility { }
-    public interface IAbilityAttack : IAbility { }
-    public interface IAbilityCounterattack : IAbility { }
-    public interface IAbilityDealDamage : IAbility { }
-    public interface IAbilityTurnEnd : IAbility { }
-}
