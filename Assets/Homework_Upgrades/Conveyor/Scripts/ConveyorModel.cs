0d2fa235b11129af26751d7cce269dadb4a54aea
Ponyu-dev <mopoypa@yandex.ru>
2024-11-01 10:29:48 +0100

    Init resources for works

--- Assets/Homework_Upgrades/Conveyor/Scripts/ConveyorModel.cs
+++ Assets/Homework_Upgrades/Conveyor/Scripts/ConveyorModel.cs
@@ -7,16 +7,5 @@ public class ConveyorModel : DeclarativeModel
     public AtomicVariable<int> LoadStorageCapacity;
     public AtomicVariable<int> UnloadStorageCapacity;
     public AtomicVariable<float> ProduceTime;
-
-    [SerializeReference] private IExample[] _examples;
-}
-
-public interface IExample
-{
-    
-}
-
-class Example : IExample
-{
 }
     
